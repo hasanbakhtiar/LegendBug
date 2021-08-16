aa65ee068d86d6496c2bfcef4bc93c931089a676
Hasan <hasan.doctype@gmail.com>
2021-08-16 12:04:39 +0400

    js lseeson1

---
+++ JS Lessons/VanillaJS/Lesson1/main.js
@@ -0,0 +1,15 @@
+//one line comment
+/*
+more line comment
+*/
+//document.write("<br>"+"Bye everyone");
+
+
+
+// alert("hello");
+// window.alert("bye");
+// debugger;
+// console.log('Hello Javascript');
+// document.write("Hello JS");
+// document.write("Hello JS");
+// document.write("Hello JS");
