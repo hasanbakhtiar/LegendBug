4ae30b233a251e0d82e29a188fc2d80b194a3824
Hasan <hasan.doctype@gmail.com>
2021-08-23 13:11:55 +0400

    add js lesson2

---
+++ JS Lessons/VanillaJS/Lesson2/dataOperatorsMiniBook.js
@@ -0,0 +1,169 @@
+
+// All Data Type 
+// --------------------------------------------------------------------------------------------------
+// Primitive (Bəsit)  Data(Data) Type(Tipi) Start
+var infomationText1   = "Salam1";     //String(mətn)      data type ("Hi")
+var infomationText2   = 'Salam2';     //String(mətn)      data type ("Hi")
+var infomationNumb    = 5 ;           //Number(rəqəm)     data type (5)
+var infomationBoolean = true ;        //Boolean(məntiqi)  data type (true/flase)
+
+document.write(infomationText1     + "<br>");
+document.write(infomationText2     + "<br>");
+document.write(infomationNumb      + "10" + "<br>");
+document.write(infomationBoolean   + "<br>");
+// Basic(Bəsit)        Data(Data) Type(Tipi) End
+// --------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------
+// Composite(Birləşik) Data(Data) Type(Tipi) Start
+var infomationArray  =  ['pc',5,true,'table'];    //Array
+var infomationObject =  {
+    name:"Samsung",
+    ROM: "OneUI"
+};                                                //Object
+
+document.write(infomationArray          + "<br>");
+document.write(infomationObject.name    + "<br>");
+document.write(infomationObject.ROM     + "<br>");
+// Composite(Birləşik) Data(Data) Type(Tipi) End
+// --------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------
+// Special(Xüsusi)     Data(Data) Type(Tipi) Start
+var informationUndefined = "";   //Undefined
+var informationNULL      = null; //Null
+
+document.write(informationUndefined          + "<br>");
+document.write(informationNULL               + "<br>");
+// Special(Xüsusi)     Data(Data) Type(Tipi) End
+// --------------------------------------------------------------------------------------------------
+
+
+// All Operators
+// --------------------------------------------------------------------------------------------------
+// Assignment Operators (Daxil Etmə operatorları) Start
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+var infoDatA = 5;
+var infoDatB = 10;
+infoDatA += infoDatB;
+document.write(infoDatA);
+// Assignment Operators (Daxil Etmə operatorları) End
+// --------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------
+// Arithmetic Operators (Riyazi operatorlar) Start
+// +    Toplama
+// -    Çıxma
+// *    Vurma
+// /    Bölmə
+// %    Qalıq
+// **   Üstü
+// ++   Bir artırma
+// --   Bir əksiltmə
+var infoNumbSt = 5; 
+var infoNumbNd = 10; 
+var infoNumbRd = 5;
+    infoNumbRd++;
+// infoNumbRd--;
+
+document.write(infoNumbSt + infoNumbNd + "<br>");
+document.write(infoNumbSt - infoNumbNd + "<br>");
+document.write(infoNumbSt * infoNumbNd + "<br>");
+document.write(infoNumbSt / infoNumbNd + "<br>");
+document.write(infoNumbSt %  infoNumbNd + "<br>");
+document.write(infoNumbSt ** infoNumbNd + "<br>");
+document.write(infoNumbRd + "<br>");
+
+
+// Arithmetic Operators (Riyazi operatorlar) End
+// --------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------
+// Comparison Operators(Qarşılaşdırma operatorları) Start
+// ==   (qoşa bərabər)
+// ===  (üçlü bərabər)
+// !=   (nida tək bərabər)
+// !==  (nida qoşa bərabər)
+// <    (kiçikdir)
+// <=   (kiçik bərabərdir)
+// >    (böyükdür)
+// >=   (böyük bərabərdir)
+
+var infoCompaSt     =  7;
+var infoCompaNd     =  7;
+
+if(infoCompaSt >= infoCompaNd){
+    document.write(true);
+}else{
+    document.write(false);
+}
+document.write("<br>");
+
+// Comparison Operators(Qarşılaşdırma operatorları) End
+// --------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------
+// Logical Operators (Məntiqi operatorlar) Start
+// && (və) true && true = true # true && false = false # true && true && false = false # true && true && true = true 
+// || (və ya) true || true = true # true || false = true # true || false || false = true # true || true || true = true 
+// ! (deyil)  !(true) = false # !(false) = true
+var infoLogiSt = true ;
+var infoLogiNd = false;
+
+
+if(!infoLogiNd){   //default true
+    document.write(true);
+}else{
+    document.write(false);
+}
+
+// Logical Operators (Məntiqi operatorlar) End
+// --------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------
+// String  Operators (Mətn operatorları) Start
+// +    (toplama string data tiplərini birləşdirir)
+// +=   (toplama bərabərdir)
+var infoStriSt = "Hi ";
+var infoStriNd = "JavaScript";
+// infoNumbSt += infoCompaNd;
+
+document.write("<br>"+ infoStriSt + infoStriNd+"<br>")
+ 
+
+// String  Operators (Mətn operatorları) End
+// --------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodalrı bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+// \            (tərs slash)(Qaçındırma operatorudur)
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+
+// ?:           (sual üst-üstə qoşa nöqtə)((şərt) ? ifadə1 : ifadə2)
+// (5 >= 10) ? document.write("Hi") : document.write("Bye");
+// şərt            ? true                 : false
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+// delete       (ojectin dəyərini silmək üçündür)
+// new          (yeni dəyər yaratməq üçündür)
+// this         (mövcud dəyəri işarələmək üçündür)
+// in           (dəyişkənin daxilində dəyərin olub olmamasını yoxlamaq üçündür)
+// instanceof   (dəyişkənin içində xüsusiyyət olub olmamasını yoxlamaq üçündür)
+
+
+
+
+
+    
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
---
+++ JS Lessons/VanillaJS/Lesson2/main.js
@@ -0,0 +1,82 @@
+// 1.1.String
+
+var infoString ='String Data Type';
+
+// console.log(infoString);
+
+
+// 1.2.Number
+var infoNumber1 = 5;
+var infoNumber2 = 15;
+// console.log(infoNumber1+infoNumber2);
+
+// 1.3.Boolean
+    // true
+    // false
+//     if(true){
+//         console.log(true + " now");
+//     }else{
+// console.log("U a doing big false");
+//     }
+
+
+// 2.1.Array
+var infoArray = [5,true,'Hello',"Front"];
+// var infoArray = new Array();
+
+// console.log(infoArray);
+
+// 2.2.Object
+
+var infoObj = {
+    name: ' Orxan',
+    age: 23,
+    eduNow: false
+};
+// var infoObj = new Object();
+// console.log(infoObj.age+infoObj.name);
+
+//3.1.Undefined
+// var infoUnd = 
+// console.log(infoUnd);
+
+var infoNull = null;
+// console.log(infoNull);
+
+
+
+// Operators
+
+// var a = '10';
+// var b = 10;
+
+// if(!false){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+// &&
+// ||
+// !
+// if(false || false || false || false || true){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+function a(name){
+    console.log(name);
+}
+
+const b = a("Hello");
+console.log( b instanceof a);
+
+
+
+
+var infoA = 10;
+console.log(delete(infoA));
+
+// 5<10 ? console.log(true) : console.log(false);;
