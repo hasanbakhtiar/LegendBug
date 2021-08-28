966970cc9b8c942f30284fb64d71ed8fa7fd4b42
Hasan Bakhtiar <52384595+HasanBakhtiar@users.noreply.github.com>
2021-08-28 20:13:28 +0400

    add js loop try switch

---
+++ JS Lessons/VanillaJS/Lesson5/main.js
@@ -0,0 +1,66 @@
+// while
+
+// let infoData = 501;
+
+// while (infoData > 1) {
+//     console.log(infoData);
+//     infoData--;
+// }
+
+
+// let infoData = 1;
+
+// while (infoData < 100) {
+//     if(infoData === 4){
+//         break;
+//     }
+//     console.log(infoData);
+//     infoData++;
+// }
+
+
+// let infoData = 1;
+
+// while (infoData < 100) {
+//     if(infoData === 4){
+//         infoData++;
+//         continue;
+//     }
+//     console.log(infoData);
+//     infoData++;
+// }
+
+// ---------------------------------------------------
+
+// do while
+// let infoData  = 1;
+// do{
+//     console.log(infoData);
+//     infoData++;
+// }while(infoData < 10);
+
+// ---------------------------------------------------
+
+
+
+// for
+// for(let infoData = 1;  infoData < 10; infoData++){
+//     console.log(infoData);
+// }
+
+// ---------------------------------------------------
+
+
+
+
+// for in
+
+// let infoArray = ["Table", 'pen', 10, true];
+// for(result in  infoArray){
+//     console.log(infoArray[result]);
+// }
+
+
+// ---------------------------------------------------
+
+
---
+++ JS Lessons/VanillaJS/Lesson6/main.js
@@ -0,0 +1,52 @@
+// const infoData = "hello";
+
+// switch(infoData){
+//     case 10:
+//         console.log("Ten");
+//     break;
+
+
+//     case "hello":
+//         console.log("Hello JavaScript"); 
+//     break;
+
+
+//     case 1:
+//         console.log("One");
+//     break;
+
+
+//     case 2:
+//         console.log("Two");
+//     break;
+
+//     case 5:
+//         console.log("Five");
+//     break;
+
+//     default:
+//         console.log("This is Dafult");
+//     break;
+
+
+
+// }
+// -----------------------------------------------------------
+
+// try
+// catch
+// throw
+// finally
+
+try{
+    throw("this is exception");
+    console.loga("This is Try Blog");
+    
+
+}catch(err){ //(error)
+        // console.log("Something wrong");
+        console.log(err);
+}finally{
+    console.log("Im runing everytime....");
+}
+
