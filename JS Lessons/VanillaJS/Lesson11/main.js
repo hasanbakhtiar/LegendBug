db0f1631988f4eb1dd98b09ec8c78f8a8cc09b99
Hasan Bakhtiar <52384595+HasanBakhtiar@users.noreply.github.com>
2021-09-06 12:23:26 +0400

    add js animation

---
+++ JS Lessons/VanillaJS/Lesson11/main.js
@@ -0,0 +1,5 @@
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_setinterval
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_setinterval_progressbar
+// https://www.w3schools.com/jsref/dom_obj_attributes.asp
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_dom_animate_3
+// https://www.w3schools.com/js/js_htmldom_events.asp
