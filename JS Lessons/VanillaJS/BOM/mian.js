0893f7eac33cd3ab584e2a42ba5f7aa1bd62354b
Hasan Bakhtiar <52384595+HasanBakhtiar@users.noreply.github.com>
2021-09-16 12:19:05 +0400

    BOM Lesson

---
+++ JS Lessons/VanillaJS/BOM/mian.js
@@ -0,0 +1,8 @@
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_win_inner
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_moveto
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_open
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_resizeto     
+// https://www.w3schools.com/js/js_window_screen.asp
+// https://www.w3schools.com/js/js_window_location.asp
+// https://www.w3schools.com/js/js_window_navigator.asp
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_prompt
