532a41c427b6c34da9bade9679cb9d453d5dd696
Hasan <hasan.doctype@gmail.com>
2021-09-20 12:49:21 +0400

    add LocalStorage and JqueryLesson1

---
+++ JS Lessons/JQuery/main.js
@@ -0,0 +1,8 @@
+// https://cdnjs.com/libraries/jquery
+// https://jquery.com/
+// https://www.w3schools.com/jquery/jquery_selectors.asp
+// https://www.w3schools.com/jquery/jquery_events.asp
+
+
+// https://www.w3schools.com/jquery/jquery_hide_show.asp
+// https://www.w3schools.com/jquery/jquery_fade.asp
