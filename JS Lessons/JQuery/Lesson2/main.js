cc20c04b73d18937257e3ab400482a5cb825de02
Hasan <hasan.doctype@gmail.com>
2021-09-23 12:59:19 +0400

    Jquery Lesson2

---
+++ JS Lessons/JQuery/Lesson2/main.js
@@ -0,0 +1,7 @@
+// localStorage.setItem("name","Nigar");
+// document.querySelector('p').innerHTML = localStorage.getItem('name');
+
+
+// https://www.w3schools.com/jquery/jquery_animate.asp
+// https://www.w3schools.com/jquery/jquery_stop.asp
+// https://www.w3schools.com/jquery/jquery_callback.asp
