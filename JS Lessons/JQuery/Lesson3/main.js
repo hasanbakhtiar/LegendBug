91a66294242c5b61fa87adae08213bf3bd142c73
Hasan <hasan.doctype@gmail.com>
2021-09-27 13:03:52 +0400

    JQ Lesson3

---
+++ JS Lessons/JQuery/Lesson3/main.js
@@ -0,0 +1,6 @@
+// https://www.w3schools.com/jquery/jquery_dom_get.asp
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_dom_attr_callback
+// https://www.w3schools.com/jquery/jquery_dom_add.asp
+// https://www.w3schools.com/jquery/jquery_css_classes.asp
+// https://www.w3schools.com/jquery/jquery_dimensions.asp
+// https://www.w3schools.com/jquery/jquery_noconflict.asp
